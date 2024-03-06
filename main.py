dd4c15412c340d984273ed6a6c02de6283c3645d
Julia Zahorovska <ju.zagorovsky@gmail.com>
2024-03-06 22:07:38 +0100

    add time_sleep between tasks

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from hw_03_1 import get_days_from_today
 from hw_03_2 import get_numbers_ticket
 from hw_03_3 import sanitized_numbers
@@ -9,13 +10,19 @@ print('Завдання 1:', '-'*40)
 get_days_from_today()
 print('\n')
 
+time.sleep(1)
+
 print('Завдання 2:', '-'*40)
 get_numbers_ticket()
 print('\n')
 
+time.sleep(1)
+
 print('Завдання 3:', '-'*40)
 sanitized_numbers(raw_numbers)
 print('\n')
+
+time.sleep(1)
 
 print('Завдання 4:', '-'*40)
 get_upcoming_birthdays(users)
